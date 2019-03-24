8e4d3455d21a036926e23dd53c39cbdfc5dbeee4
MiriamAsi <44280688+MiriamAsi@users.noreply.github.com>
2019-03-23 18:29:49 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+// Fig. 4.1: Gradebook.cs
+//Class declaration with one method.
+using System;
+
+public class GradeBook
+{
+    //display a welcome message to the GradeBook user
+    public void DisplayMessage()
+    {
+        Console.WriteLine( "Hello World from Miriam!" );
+    } // end method DisplayMessage
+} // end class GradeBook
